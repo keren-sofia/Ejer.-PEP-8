d4a2c28985dbae8f41ba4a4b3be6fc27f5491e60
keren-sofia <84553085+keren-sofia@users.noreply.github.com>
2021-05-23 22:45:04 -0500

    Add files via upload

---
+++ Ejer. PEP 8.py
@@ -0,0 +1,17 @@
+'''
+Ejercicio incorrecto:
+
+n = 10 if n> 5: print "n es grande"
+
+Errores: 
+- no contiene espacio en blanco entre n y el signo >
+- no tiene buena codificación la función print, ya que le faltan parentesis
+- no tiene indentaación para la función print, que debe ir abajo de la función if
+- no son separados los bloques, los cuales son variables y condicional 
+'''
+'''
+Ejercicio correcto:
+'''
+n = 10
+if n > 5:
+   print("n es grande")
